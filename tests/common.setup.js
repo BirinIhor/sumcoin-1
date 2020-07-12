d96be1af5cf9d1a35b0f7fd10d58adc1df4a13fc
BirinIhor <56085311+BirinIhor@users.noreply.github.com>
2020-07-12 23:45:21 +0200

    Initialize feature/tests-in-ci

---
+++ tests/common.setup.js
@@ -0,0 +1,90 @@
+import Controller from './websocket-client';
+import TrezorConnect, { UI } from '../src/js/index';
+
+// TrezorConnect and Controller should be imported and initialized in one file
+// otherwise karma will put "TrezorConnect" instance into every *.test file
+console.log('common setup');
+const MNEMONICS = {
+    'mnemonic_all': 'all all all all all all all all all all all all',
+    'mnemonic_12': 'alcohol woman abuse must during monitor noble actual mixed trade anger aisle',
+};
+
+// let _popup;
+// window.addEventListener("message", m => {
+//     console.log("Catch message", m, POPUP)
+// });
+// window.open = function (open) {
+//     return function (url, name, features) {
+//         console.log("WINDOW OPEN", url, name, features)
+//         // set name if missing here
+//         name = name || "default_window_name";
+//         _popup = open.call(window, url, name, features)
+//         _popup.onload = () => {
+//             console.log("POPUP ON LOAD!")
+//         }
+//         _popup.addEventListener("message", m => {
+//             console.log("POPUP ON LOAD!")
+//         });
+//         console.log("CATCH HIS MESS", _popup.locati, _popup.document.body)
+//         return _popup;
+//     };
+// }(window.open);
+
+const setup = async (controller, options) => {
+    controller.on('connect', () => {
+
+    });
+    await controller.connect();
+    await controller.send({ type: 'bridge-start' });
+    await controller.send({ type: 'emulator-start', version: '2.2.0' });
+
+    if (options.wipe) {
+        await controller.send({ type: 'emulator-wipe' });
+    } else {
+        const mnemonic = typeof options.mnemonic === 'string' && options.mnemonic.indexOf(' ') > 0 ? options.mnemonic : MNEMONICS[options.mnemonic];
+
+        await controller.send({
+            type: 'emulator-setup',
+            mnemonic,
+            pin: options.pin || '',
+            passphrase_protection: false,
+            label: options.label || 'TrezorT',
+            backup: false,
+            options,
+        });
+    }
+};
+
+const initTrezorConnect = async (controller, options) => {
+    await TrezorConnect.init({
+        connectSrc: 'http://localhost:8099/_karma_webpack_/',
+        manifest: {
+            appUrl: 'a',
+            email: 'b',
+        },
+        webusb: false,
+        debug: false,
+        // lazyLoad: true,
+        popup: false,
+        // pendingTransportEvent: false,
+        ...options,
+    });
+
+    TrezorConnect.on(UI.REQUEST_CONFIRMATION, () => {
+        TrezorConnect.uiResponse({
+            type: UI.RECEIVE_CONFIRMATION,
+            payload: true,
+        });
+    });
+
+    TrezorConnect.on(UI.REQUEST_BUTTON, async (event) => {
+        await controller.send({ type: 'emulator-decision', method: controller.options.name });
+    });
+};
+
+global.Trezor = {
+    setup,
+    initTrezorConnect,
+    TrezorConnect,
+    Controller,
+};
